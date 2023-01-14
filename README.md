491824da662da44eb41b6fc8ceb5aa49a8090722
JoeAttard90 <attard999@gmail.com>
2023-01-14 18:52:56 +0000

    fix: finalised the basic structure and addressed the go mod issues

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # loggit
+
+This pkg is for easy logging of errors and information. It uses the log standard flags:
--- pkg/loggit/loggit.go
+++ pkg/loggit/loggit.go
@@ -2,6 +2,7 @@ package loggit
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"log"
 	"os"
 )
@@ -18,17 +19,18 @@ type Loggit struct {
 
 func NewLoggit() *Loggit {
 	return &Loggit{
-		logger: log.New(os.Stderr, "", log.LstdFlags|log.Llongfile),
+		correlationID: uuid.NewString(),
+		logger:        log.New(os.Stderr, "", log.LstdFlags),
 	}
 }
 
 func (l *Loggit) Error(body string, err error, variables ...any) {
 	variables = append(variables, err)
-	logFmt := fmt.Sprintf("[ERROR] %s -- %s: %s", l.correlationID, body, "%v")
-	l.logger.Output(4, fmt.Sprintf(logFmt, variables))
+	logFmt := fmt.Sprintf("[ERROR] txid:%s: %s: %s", l.correlationID, body, "%v")
+	l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
 }
 
 func (l *Loggit) Info(body string, variables ...any) {
-	logFmt := fmt.Sprintf("[INFO] %s -- %s: %s", l.correlationID, body, "%v")
-	l.logger.Output(4, fmt.Sprintf(logFmt, variables))
+	logFmt := fmt.Sprintf("[INFO]: txid:%s: %s", l.correlationID, body)
+	l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
 }
