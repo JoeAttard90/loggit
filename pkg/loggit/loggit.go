f45c8c2803787da0ea20c16198e58122324bbeeb
JoeAttard90 <attard999@gmail.com>
2023-01-15 10:49:23 +0000

    task: fixed the log format if the txID is not present

--- pkg/loggit/loggit.go
+++ pkg/loggit/loggit.go
@@ -32,11 +32,22 @@ func NewLoggitWithoutTXID() *Loggit {
 
 func (l *Loggit) Error(body string, err error, variables ...any) {
 	variables = append(variables, err)
-	logFmt := fmt.Sprintf("[ERROR] txid:%s: %s: %s", l.txID, body, "%v")
+	if l.txID != "" {
+		logFmt := fmt.Sprintf("[ERROR] txid:%s: %s: %s", l.txID, body, "%v")
+		l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
+		return
+	}
+	logFmt := fmt.Sprintf("[ERROR] %s: %s", body, "%v")
 	l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
 }
 
 func (l *Loggit) Info(body string, variables ...any) {
-	logFmt := fmt.Sprintf("[INFO]: txid:%s: %s", l.txID, body)
+	if l.txID != "" {
+		logFmt := fmt.Sprintf("[INFO]: txid:%s: %s", l.txID, body)
+		l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
+		return
+	}
+	logFmt := fmt.Sprintf("[INFO] %s", body)
 	l.logger.Output(4, fmt.Sprintf(logFmt, variables...))
+
 }
